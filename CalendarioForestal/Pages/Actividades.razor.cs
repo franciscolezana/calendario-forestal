ce6d1bcc2c0059e4eef3a6eb2f333eae87eb28c8
Francisco Lezana Ramos <30421193+franciscolezana@users.noreply.github.com>
2023-08-07 23:45:33 -0600

    feat: views without services

---
+++ CalendarioForestal/Pages/Actividades.razor.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace CalendarioForestal.Pages
+{
+    public partial class Actividades
+    {
+        protected int count;
+        protected bool isEdit = true;
+
+        public class DataItem
+        {
+            public string Start { get; set; } = "";
+            public string End { get; set; } = "";
+            public string Nombre { get; set; } = "";
+            public string Detalles { get; set; } = "";
+            public string FaseLunar { get; set; } = "";
+            public string Epoca { get; set; } = "";
+            public string Tiempo { get; set; } = "";
+            public string Arbol { get; set; } = "";
+            public string CambioSol { get; set; } = "";
+            public string Cuatrimestre { get; set; } = "";
+        }
+
+        [Parameter]
+        public DateTime Start { get; set; }
+
+        [Parameter]
+        public DateTime End { get; set; }
+
+        [Parameter]
+        public string Nombre { get; set; } = "";
+
+        [Parameter]
+        public string Detalles { get; set; } = "";
+
+        [Parameter]
+        public string FaseLunar { get; set; } = "";
+
+        [Parameter]
+        public string Epoca { get; set; } = "";
+
+        [Parameter]
+        public string Tiempo { get; set; } = "";
+
+        [Parameter]
+        public string Arbol { get; set; } = "";
+
+        [Parameter]
+        public string CambioSol { get; set; } = "";
+
+        [Parameter]
+        public string Cuatrimestre { get; set; } = "";
+
+        DataItem model = new DataItem();
+
+        protected override void OnParametersSet()
+        {
+            model.Start = Start.AddDays(-1).ToString("dd/MM/yyyy");
+            model.End = End.AddDays(1).ToString("dd/MM/yyyy");
+            model.Nombre = Nombre;
+            model.Detalles = Detalles;
+            model.FaseLunar = FaseLunar;
+            model.Epoca = Epoca;
+            model.Tiempo = Tiempo;
+            model.Arbol = Arbol;
+            model.CambioSol = CambioSol;
+            model.Cuatrimestre = Cuatrimestre;
+
+        }
+    }
+}
--- CalendarioForestal/Program.cs
+++ CalendarioForestal/Program.cs
@@ -1,11 +1,18 @@
 using CalendarioForestal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+var host = builder.Build();
+var jsRuntime = host.Services.GetRequiredService<Microsoft.JSInterop.IJSRuntime>();
+var app = builder.Build();
 await builder.Build().RunAsync();
+
